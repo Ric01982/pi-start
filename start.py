616c45e16864631f8f77f2dacb301c6f78a1d6e9
Ric01982 <ikrichardson@gmail.com>
2022-07-08 20:02:52 +0100

    2

--- start.py
+++ start.py
@@ -1,11 +1,12 @@
 import time
 import urllib.request
 
-version = 1
+version = 2
 
 while 1 < 2 :
+        urllib.request.urlretrieve("https://github.com/Ric01982/pi-start/raw/main/start.py","/usr/local/src/start.py")
         time.sleep(30)
-        urllib.request.urlretrieve("https://github.com/Ric01982/pi-start/raw/main/start.py","/usr/local/svc/start.py")
+
 
 
 
