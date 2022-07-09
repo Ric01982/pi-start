2e36165497a22fa3d441b61fa4fb12fa094e8158
Ric01982 <ikrichardson@gmail.com>
2022-07-09 17:40:54 +0100

    1

--- start.py
+++ start.py
@@ -4,19 +4,18 @@ import time
 version = "0907221402"
 scriptlist = (open("/usr/local/src/scriptlist.dat","r")).read()
 
+urllib.request.urlretrieve("https://github.com/Ric01982/pi-start/raw/main/scriptlist.dat","/usr/local/src/scriptlist.dat")
 
 while True:
-        urllib.request.urlretrieve("https://github.com/Ric01982/pi-start/raw/main/scriptlist.dat","/usr/local/src/scriptlist.dat")
-        
+                
         for script in scriptlist:
                 
-                urllib.request.urlretrieve("https://github.com/Ric01982/pi-start/raw/main/"&script,"/usr/local/src/"&script)
+                urllib.request.urlretrieve("https://github.com/Ric01982/pi-start/raw/main/{script}","/usr/local/src/{script}")
 
         time.sleep(30)
 
 
-
-
+ 
 
 
 
